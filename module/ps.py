c95b7645c7f16bc2bf1470bfc001ff00ee2e5cf3
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-10-12 17:50:13 -0400

    Update ps.py

--- module/ps.py
+++ module/ps.py
@@ -27,11 +27,14 @@ class PsModule(ModuleObject):
 			
         path = self.write_file(stage1)
 	letter = random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase)
+	letter2 = random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase)
         stage1 = self.read_file(path).replace(letter, "!")
+	stage1 = self.read_file(path).replace(letter2, "$")
         self.delete_file(path)
 	
         stage2 = self.load_file("core/util/base64.ps1").replace("[BASE64]", stage1)
-	stage2 = stage2.replace("[CHAR]", letter)
+	stage2 = stage2.replace("[CHAR1]", letter)
+	stage2 = stage2.replace("[CHAR2]", letter2)
 	for i in range(1, 3):
                 stage2 = stage2.replace("VAR" + str(i), self.gen_str(random.randrange(3, 10)))
 		
