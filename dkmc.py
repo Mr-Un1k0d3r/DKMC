cd0bb4480574fec63d603622c465702b027a98da
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2020-05-01 16:17:03 -0400

    Update dkmc.py

--- dkmc.py
+++ dkmc.py
@@ -17,33 +17,33 @@ if __name__ == "__main__":
     options.append(("ps", "Generate Powershell payload"))
     options.append(("sc", "Generate shellcode from raw file"))
     options.append(("exit", "Quit the application"))
-    
+
     exit_loop = False
     error = ""
     ui = UI()
     menu = MenuUI()
-    
+
     while not exit_loop:
         try:
-	        ui.banner()
-        	choice = ui.show_menu(options, error)
-	        error = ""
-        	if menu.is_an_option(choice):
-	            mod = None
-        	    if choice == "exit":
-                	exit(0)
-	            if choice == "gen":
-        	        mod = GenModule(ui)
-	            if choice == "web":
-        	        mod = WebModule(ui)
-	            if choice == "ps":
-        	        mod = PsModule(ui)
-	            if choice == "sc":
-        	        mod = ShellcodeModule(ui)
-                    mod.show_menu()
-        	else:
-            		error = "%s is not a valid option" % choice
-	except KeyboardInterrupt:
-		print ""
-		ui.print_error("Exiting")
-		exit(0)
+            ui.banner()
+            choice = ui.show_menu(options, error)
+            error = ""
+            if menu.is_an_option(choice):
+                mod = None
+                if choice == "exit":
+                    exit(0)
+                if choice == "gen":
+                    mod = GenModule(ui)
+                if choice == "web":
+                    mod = WebModule(ui)
+                if choice == "ps":
+                    mod = PsModule(ui)
+                if choice == "sc":
+                    mod = ShellcodeModule(ui)
+                mod.show_menu()
+            else:
+                error = "%s is not a valid option" % choice
+        except KeyboardInterrupt:
+            print ""
+            ui.print_error("Exiting")
+            exit(0)
